1ee6ee054132c7925b125d4bb467c8e5fad7a127
Diana Castilleja <89886959+Dianacastilleja@users.noreply.github.com>
2023-11-07 13:43:30 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Function to read in details for page
+def readDetails(filepath):
+    with open('LabFlask/static/aboutMe.txt', 'r') as f:
+        return [line for line in f]
+
+@app.route('/')
+def homepage():
+    about_me_contents = readDetails('aboutMe.txt')
+    return render_template('base.html', aboutMe=about_me_contents)
+
+@app.route('/user/<name>')
+def hello(name):
+    return render_template('hello.html', name = name)
+
+@app.route('/form', methods=['GET', 'POST'])
+def formDemo():
+    name = None
+    if request.method == 'POST':
+        name=request.form['name']
+    return render_template('form.html', name=name)
+
+# Add the option to run the file directly
+if __name__ == "__main__":
+    app.run(debug=True)
